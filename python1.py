767e8c003aa3c6398eac49c16c935d6736b2635c
tasneemalbwshy850-ship-it <tasneemalbwshy850@gmail.com>
2025-08-30 18:58:51 +0300

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,22 @@
+tomatoes = 20
+apples = 40
+potatoes = 30
+total_money = 100
+
+T = tomatoes+potatoes+apples
+C = total_money % T
+D = total_money-T
+
+print("Tomatoes:",tomatoes)
+print("Apples:",apples) 
+print("Potatoes:",potatoes)
+print("Total_money:",total_money)
+print("total_items:",T)
+print("the remaining_money:",D)
+print("the difference:",C)
+
+
+
+
+
+

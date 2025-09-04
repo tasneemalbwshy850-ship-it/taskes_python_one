54e4c1b0cdf7e15cf8ed9423e750bab1b2aed3a9
tasneemalbwshy850-ship-it <tasneemalbwshy850@gmail.com>
2025-09-04 20:36:31 +0300

    Add files via upload

---
+++ python2_(2) temperature.py
@@ -0,0 +1,9 @@
+x = 25
+if x >= 30 :
+    print ("It's a hot day. Stay hydrated!")
+elif 29 >= x >= 20 :
+    print ("It's a warm day. Enjoy the weather!")
+elif 19 >= x >= 10 :
+    print ("It's a cool day. Wear a jacket!")
+else :
+    print ("It's a cold day. Stay warm!")        

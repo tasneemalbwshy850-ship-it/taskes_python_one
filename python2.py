d45ce1bcab30993fe4c0bfd1b4892e16910c33ea
tasneemalbwshy850-ship-it <tasneemalbwshy850@gmail.com>
2025-09-04 20:34:35 +0300

    Delete python2.py

--- python2.py
+++
@@ -1,7 +0,0 @@
-number_list =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-for number in number_list:
-    if number % 3 == 0 :
-        print(number)
-
-
-# This code prints all numbers in the list that are divisible by 3.
